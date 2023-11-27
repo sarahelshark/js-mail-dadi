ed027811fa0446fc33c42ae59de40d4d07c0a6f9
sarahelshark <sarahelsh183@gmail.com>
2023-11-27 15:49:32 +0100

    feat add
     tools to human coding

--- myscript.js
+++ myscript.js
@@ -1,1 +1,29 @@
 console.log("im working");
+/*
+Mail
+------
+Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.
+
+>tools
+-form con input e bottone submit
+-event listener
+-let/const
+-query selector o getElementById
+-condizionale
+-stampo nel dom .innerHTML
+
+*/
+
+/*
+Gioco dei dadi
+------
+Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+>tools
+-form con input e bottone submit
+-event listener
+-let/const
+-query selector o getElementById
+-condizionale
+
+*/
