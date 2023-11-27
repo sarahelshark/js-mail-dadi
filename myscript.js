b5409af1a04f5f68a44f0bb3798a9bf3ddde32f5
sarahelshark <sarahelsh183@gmail.com>
2023-11-27 15:54:38 +0100

    feat add mail human code

--- myscript.js
+++ myscript.js
@@ -6,11 +6,18 @@ Chiedi all’utente la sua email, controlla che sia nella lista di chi può acce
 
 >tools
 -form con input e bottone submit
--event listener
+-event listener (oppure prompt, piu facile ma piu brutto)
 -let/const
+-array
 -query selector o getElementById
 -condizionale
 -stampo nel dom .innerHTML
+
+>steps
+1.creare una lista di email
+2.chiedere all'utente di inserire la propria mail
+3. confrontare la mail inserita dall'utente con tutte le email inserite nella lista
+4. se la mail è presente nella lista l'utente può accedere, se non è presente l'utente viene bloccato
 
 */
 
